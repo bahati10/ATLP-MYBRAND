d169dc82c3e0841571df9f039c2ce1959b8f1bc4
Bahati Yves <bahatiyves10@gmail.com>
2024-03-14 10:03:43 +0200

    Fixing route protection isuues

---
+++ js/protect-add-blog.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('DOM content loaded.');
+    if (window.location.pathname === "/addblog.html") {
+        checkAuth();
+    }
+});
+
+
+function checkAuth() {
+    const isLoggedIn = sessionStorage.getItem('isLoggedIn');
+    const userEmail = sessionStorage.getItem('userEmail');
+    
+    const expectedUserEmail = 'admin@gmail.com';
+
+    console.log('isLoggedIn:', isLoggedIn);
+    console.log('userEmail:', userEmail);
+
+    if (userEmail !== expectedUserEmail) {
+        console.log('Unauthorized access detected. Redirecting to login page.');
+        window.location.href = "blog.html";
+    }
+}
